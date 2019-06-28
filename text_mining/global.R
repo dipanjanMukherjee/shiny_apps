34e381015af7c346cb5fbc6f4c575419f8bf3cb9
dipanjanMukherjee <dipanjan_mukhopadhyay@outlook.com>
2019-06-28 21:01:29 +0530

    Create global.R

---
+++ text_mining/global.R
@@ -0,0 +1,13 @@
+library(shiny)
+library(shinydashboard)
+library(tm)
+library(lsa)
+library(wordcloud)
+library(gplots)
+library(plotly)
+
+library(NLP)
+#library(openNLP)
+#library(qdap)
+
+library(RColorBrewer)
